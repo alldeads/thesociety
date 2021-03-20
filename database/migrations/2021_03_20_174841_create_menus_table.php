be7f2b0fd6eb72fd41b236d1fdb88ec338c86ca6
John Rexter Flores <johnrexter.flores@gmail.com>
2021-03-21 01:54:00 +0800

    Create menu model and migration

---
+++ database/migrations/2021_03_20_174841_create_menus_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('header_id')->unsigned();
+            $table->string('name');
+            $table->string('slug')->nullable();
+            $table->string('url')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('badge')->nullable();
+            $table->string('badgeClass')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
