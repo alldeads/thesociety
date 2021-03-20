ae835b4877b984cba3535c6e247ac33e9b8854a1
John Rexter Flores <johnrexter.flores@gmail.com>
2021-03-21 00:13:58 +0800

    Fix layout master file

--- resources/views/layouts/fullLayoutMaster.blade.php
+++ resources/views/layouts/fullLayoutMaster.blade.php
@@ -1,62 +1,58 @@
 @isset($pageConfigs)
-{!! Helper::updatePageConfig($pageConfigs) !!}
+    {!! Helper::updatePageConfig($pageConfigs) !!}
 @endisset
 
 <!DOCTYPE html>
-{{-- {!! Helper::applClasses() !!} --}}
-@php
-$configData = Helper::applClasses();
-@endphp
-<html lang="@if(session()->has('locale')){{session()->get('locale')}}@else{{$configData['defaultLanguage']}}@endif" data-textdirection="{{ env('MIX_CONTENT_DIRECTION') === 'rtl' ? 'rtl' : 'ltr' }}" class="{{ ($configData['theme'] === 'light') ? '' : $configData['layoutTheme'] }}">
+    @php
+        $configData = Helper::applClasses();
+    @endphp
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <html lang="@if(session()->has('locale')){{session()->get('locale')}}@else{{$configData['defaultLanguage']}}@endif" data-textdirection="{{ env('MIX_CONTENT_DIRECTION') === 'rtl' ? 'rtl' : 'ltr' }}" class="{{ ($configData['theme'] === 'light') ? '' : $configData['layoutTheme'] }}">
 
-  <title>@yield('title') - Vuexy Vuejs, HTML & Laravel Admin Dashboard Template</title>
-  <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/logo/favicon.ico')}}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  {{-- Include core + vendor Styles --}}
-  @include('panels/styles')
+        <title>
+            @yield('title') - The Society 32
+        </title>
 
-  {{-- Include core + vendor Styles --}}
-  @include('panels/styles')
-</head>
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/logo/favicon.ico')}}">
 
+        {{-- Include core + vendor Styles --}}
+        @include('panels/styles')
 
+        {{-- Include core + vendor Styles --}}
+        @include('panels/styles')
+    </head>
 
-<body class="vertical-layout vertical-menu-modern {{ $configData['blankPageClass'] }} {{ $configData['bodyClass'] }} {{($configData['theme'] === 'dark') ? 'dark-layout' : 'light' }}
+    <body class="vertical-layout vertical-menu-modern {{ $configData['blankPageClass'] }} {{ $configData['bodyClass'] }} {{($configData['theme'] === 'dark') ? 'dark-layout' : 'light' }}
     data-menu=" vertical-menu-modern" data-layout="{{ ($configData['theme'] === 'light') ? '' : $configData['layoutTheme'] }}" style="{{ $configData['bodyStyle'] }}" data-framework="laravel" data-asset-path="{{ asset('/')}}">
 
-  <!-- BEGIN: Content-->
-  <div class="app-content content {{ $configData['pageClass'] }}">
-    <div class="content-wrapper {{ $configData['layoutWidth'] === 'boxed' ? 'container p-0' : '' }}">
-      <div class="content-body">
+        <!-- BEGIN: Content-->
+        <div class="app-content content {{ $configData['pageClass'] }}">
+            <div class="content-wrapper {{ $configData['layoutWidth'] === 'boxed' ? 'container p-0' : '' }}">
+                <div class="content-body">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+        <!-- End: Content-->
 
-        {{-- Include Startkit Content --}}
-        @yield('content')
+        {{-- include default scripts --}}
+        @include('panels/scripts')
 
-      </div>
-    </div>
-  </div>
-  <!-- End: Content-->
-
-  {{-- include default scripts --}}
-  @include('panels/scripts')
-
-  <script type="text/javascript">
-    $(window).on('load', function() {
-      if (feather) {
-        feather.replace({
-          width: 14
-          , height: 14
-        });
-      }
-    })
-
-  </script>
-</body>
-
+        <script type="text/javascript">
+            $(window).on('load', function() {
+                if (feather) {
+                    feather.replace({
+                        width: 14,
+                        height: 14
+                    });
+                }
+            })
+        </script>
+    </body>
 </html>
