988ea3ebfb850097194023fc3e5b4db53c7f2edc
John Rexter Flores <johnrexter.flores@gmail.com>
2021-03-22 01:14:10 +0800

    Install roles and permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CompanySeeder::class,
         	UserSeeder::class,
             MenuSeeder::class,
             ChartAccountSeeder::class
