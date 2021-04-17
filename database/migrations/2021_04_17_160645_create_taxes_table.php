259506ade0f2dc4f8bc50d6095c2d2fbf989778b
John Rexter Flores <johnrexter.flores@gmail.com>
2021-04-18 00:22:37 +0800

    Create tax model and migration

---
+++ database/migrations/2021_04_17_160645_create_taxes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('company_id')->unsigned();
+            $table->string('name');
+            $table->decimal('fixed_rate', 12, 2)->nullable();
+            $table->decimal('percentage', 12, 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxes');
+    }
+}
