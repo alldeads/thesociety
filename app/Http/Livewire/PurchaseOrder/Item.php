08a8c9296246a54a8ac792a5a89f2b4491010f4b
John Rexter Flores <johnrexter.flores@gmail.com>
2021-04-19 03:39:47 +0800

    Create approve function for purchase orders

--- app/Http/Livewire/PurchaseOrder/Item.php
+++ app/Http/Livewire/PurchaseOrder/Item.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Livewire\PurchaseOrder;
 
-use Livewire\Component;
+use App\Http\Livewire\CustomComponent;
 
-class Item extends Component
+use App\Models\PurchaseOrder;
+use App\Models\Status;
+
+class Item extends CustomComponent
 {
 	public $item;
+
+	public function approve()
+	{
+		$po = PurchaseOrder::find($this->item->id);
+		$status = Status::where('name', 'approved')->first();
+
+		$po->status_id = $status->id;
+		$po->updated_by = auth()->id();
+		$po->save();
+
+		$this->item = $po;
+
+		$this->emitSelf('$refresh');
+
+		$this->message('Success! Purchase Order has been approved.', 'success');
+	}
 	
     public function render()
     {
