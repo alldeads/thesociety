dcdb0b4f395d3a9beee124ca01feba7ff6550e98
John Rexter Flores <johnrexter.flores@gmail.com>
2021-06-05 22:05:34 +0800

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -1,86 +1,69 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WebController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\AccountingController;
-use App\Http\Controllers\ChartOfAccountController;
-use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\CashFlowController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\SupplierController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SupplyController;
-use App\Http\Controllers\TaxController;
-use App\Http\Controllers\PurchaseOrderController;
-
 Auth::routes(['register' => false]);
-
 	
-Route::get('/', [WebController::class, 'index'])->name('index');
+Route::get('/', 'WebController@index')->name('index');
 
 Route::middleware('auth')->group(function() {
 
-	Route::get('home', [DashboardController::class, 'index'])->name('home');
+	Route::get('home', 'DashboardController@index')->name('home');
 
 	Route::prefix('company')->group( function() {
-		Route::get('details', [CompanyController::class, 'details'])->name('company-details');
-		Route::get('edit', [CompanyController::class, 'edit'])->name('company-edit');
+		Route::get('details', 'CompanyController@details')->name('company-details');
+		Route::get('edit', 'CompanyController@edit')->name('company-edit');
 	});
 
 	Route::prefix('employees')->group( function() {
-		Route::get('view', [EmployeeController::class, 'get_all'])->name('employees-view');
-		Route::get('create', [EmployeeController::class, 'create'])->name('employees-create');
-		Route::get('edit/{emp}', [EmployeeController::class, 'edit'])->name('employees-edit');
+		Route::get('view', 'EmployeeController@get_all')->name('employees-view');
+		Route::get('create', 'EmployeeController@create')->name('employees-create');
+		Route::get('edit/{emp}', 'EmployeeController@edit')->name('employees-edit');
 	});
 
 	Route::prefix('customers')->group( function() {
-		Route::get('view', [CustomerController::class, 'index'])->name('customers-view');
-		Route::get('create', [CustomerController::class, 'create'])->name('customers-create');
-		Route::get('view/{customer}', [CustomerController::class, 'view'])->name('customers-read');
-		Route::get('edit/{customer}', [CustomerController::class, 'edit'])->name('customers-edit');
+		Route::get('view', 'CustomerController@index')->name('customers-view');
+		Route::get('create', 'CustomerController@create')->name('customers-create');
+		Route::get('view/{customer}', 'CustomerController@view')->name('customers-read');
+		Route::get('edit/{customer}', 'CustomerController@edit')->name('customers-edit');
 	});
 
 	Route::prefix('products')->group( function() {
-		Route::get('view', [ProductController::class, 'index'])->name('products-view');
-		Route::get('create', [ProductController::class, 'create'])->name('products-create');
-		Route::get('view/{product}', [ProductController::class, 'view'])->name('products-read');
-		Route::get('edit/{product}', [ProductController::class, 'edit'])->name('products-edit');
+		Route::get('view', 'ProductController@index')->name('products-view');
+		Route::get('create', 'ProductController@create')->name('products-create');
+		Route::get('view/{product}', 'ProductController@view')->name('products-read');
+		Route::get('edit/{product}', 'ProductController@edit')->name('products-edit');
 	});
 
 	Route::prefix('supplies')->group( function() {
-		Route::get('view', [SupplyController::class, 'index'])->name('supplies-view');
-		Route::get('create', [SupplyController::class, 'create'])->name('supplies-create');
-		Route::get('view/{product}', [SupplyController::class, 'view'])->name('supplies-read');
-		Route::get('edit/{product}', [SupplyController::class, 'edit'])->name('supplies-edit');
+		Route::get('view', 'SupplyController@index')->name('supplies-view');
+		Route::get('create', 'SupplyController@create')->name('supplies-create');
+		Route::get('view/{product}', 'SupplyController@view')->name('supplies-read');
+		Route::get('edit/{product}', 'SupplyController@edit')->name('supplies-edit');
 	});
 
 	Route::prefix('inventory')->group( function() {
 		Route::prefix('suppliers')->group( function() {
-			Route::get('view', [SupplierController::class, 'index'])->name('suppliers-view');
-			Route::get('create', [SupplierController::class, 'create'])->name('suppliers-create');
-			Route::get('view/{supplier}', [SupplierController::class, 'view'])->name('suppliers-read');
-			Route::get('edit/{supplier}', [SupplierController::class, 'edit'])->name('suppliers-edit');
+			Route::get('view', 'SupplierController@index')->name('suppliers-view');
+			Route::get('create', 'SupplierController@create')->name('suppliers-create');
+			Route::get('view/{supplier}', 'SupplierController@view')->name('suppliers-read');
+			Route::get('edit/{supplier}', 'SupplierController@edit')->name('suppliers-edit');
 		});
 
 		Route::prefix('purchase-orders')->group( function() {
-			Route::get('view', [PurchaseOrderController::class, 'index'])->name('purchase-orders-view');
-			Route::get('download/{purchase}', [PurchaseOrderController::class, 'download'])->name('purchase-orders-download');
-			Route::get('create', [PurchaseOrderController::class, 'create'])->name('purchase-orders-create');
-			Route::get('view/{purchase}', [PurchaseOrderController::class, 'view'])->name('purchase-orders-read');
-			Route::get('edit/{purchase}', [PurchaseOrderController::class, 'edit'])->name('purchase-orders-edit');
+			Route::get('view', 'PurchaseOrderController@index')->name('purchase-orders-view');
+			Route::get('download/{purchase}', 'PurchaseOrderController@download')->name('purchase-orders-download');
+			Route::get('create', 'PurchaseOrderController@create')->name('purchase-orders-create');
+			Route::get('view/{purchase}', 'PurchaseOrderController@view')->name('purchase-orders-read');
+			Route::get('edit/{purchase}', 'PurchaseOrderController@edit')->name('purchase-orders-edit');
 		});
 	});
 
 	Route::prefix('accounting')->group( function() {
-		Route::get('chart-of-accounts', [ChartOfAccountController::class, 'index'])->name('chart-of-accounts');
-		Route::get('cash-flow', [CashFlowController::class, 'index'])->name('cash-flow');
-		Route::get('tax', [TaxController::class, 'index'])->name('tax');
+		Route::get('chart-of-accounts', 'ChartOfAccountController@index')->name('chart-of-accounts');
+		Route::get('cash-flow', 'CashFlowController@index')->name('cash-flow');
+		Route::get('tax', 'TaxController@index')->name('tax');
 	});
 
 	Route::prefix('roles')->group( function() {
-		Route::get('view', [RoleController::class, 'get_all'])->name('roles-view');
+		Route::get('view', 'RoleController@get_all')->name('roles-view');
 	});
 });
