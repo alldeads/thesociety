df04d734fa1d9cf843f7d1a695ff316084f49702
John Rexter Flores <johnrexter.flores@gmail.com>
2021-06-08 01:06:59 +0800

    Update cash flow table layout

--- resources/views/livewire/cash-flow/item.blade.php
+++ resources/views/livewire/cash-flow/item.blade.php
@@ -29,20 +29,38 @@
 	<td>{{ number_format($item->balance, 2,'.', ',') }}</td>
 
 	@if( auth()->user()->can('cashflow.update') || auth()->user()->can('cashflow.delete') )
-		<td>
-		    @can('cashflow.update')
-				<span type="button" wire:click="edit">
-					<i class="fas fa-pen ml-1"></i>
-				</span>
-		    @endcan
 
-		    @if ($last->id == $item->id)
-		    	@can('cashflow.delete')
-		    		<span type="button" wire:click="delete">
-						<i class="fas fa-trash ml-1"></i>
-					</span>
-			    @endcan
-			@endif
+		<td>
+			<div class="dropdown">
+                <button type="button" class="btn btn-sm dropdown-toggle hide-arrow" data-toggle="dropdown">
+                   	<i class="fas fa-ellipsis-v ml-1"></i>
+                </button>
+
+                <div class="dropdown-menu">
+                	@can('cashflow.update')
+	                    <a class="dropdown-item" wire:click="edit" href="javascript:void(0);">
+	                      	<i class="fas fa-pen mr-1"></i>
+	                      	<span>Edit</span>
+	                    </a>
+	                @endcan
+
+	                {{-- @can('chart.read')
+	                    <a class="dropdown-item" wire:click="read" href="javascript:void(0);">
+	                      	<i class="fas fa-eye mr-1"></i>
+	                      	<span>View</span>
+	                    </a>
+	                @endcan --}}
+
+	                @if ($last->id == $item->id)
+		                @can('cashflow.delete')
+		                    <a class="dropdown-item" wire:click="delete" href="javascript:void(0);">
+		                      	<i class="fas fa-trash mr-1"></i>
+		                      	<span>Delete</span>
+		                    </a>
+		                @endcan
+	                @endif
+                </div>
+            </div>
 		</td>
 	@endif
 </tr>
