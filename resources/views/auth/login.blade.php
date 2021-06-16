749eea63b231a16bd117b3257da3cf9072cac41a
John Rexter Flores <johnrexter.flores@gmail.com>
2021-06-17 00:53:04 +0800

    Refactor login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,6 @@
 @section('title', 'Login')
 
 @section('page-style')
-    {{-- Page Css files --}}
-    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('css/base/pages/page-auth.css')) }}">
 @endsection
 
@@ -37,12 +35,18 @@
             <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
                     <img src="{{ asset('images/logo/logo.png') }}" width="300">
-                    <p class="card-text mb-2">Please sign-in to your account and start the adventure</p>
+                    <p class="card-text mb-2">
+                        {{ __('Please sign-in to your account and start the adventure') }}
+                    </p>
                     <form class="auth-login-form mt-2" action="{{ route('login') }}" method="POST">
                         @csrf
                         <div class="form-group">
-                            <label class="form-label" for="login-email">Email</label>
+                            <label class="form-label" for="login-email">
+                                {{ __('Email') }}
+                            </label>
+
                             <input class="form-control @error('email') is-invalid @enderror" id="login-email" type="email" name="email" placeholder="john@example.com" aria-describedby="login-email" autofocus="" tabindex="1" value="{{ old('email') }}" required />
+
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -52,9 +56,9 @@
 
                         <div class="form-group">
                             <div class="d-flex justify-content-between">
-                                <label for="login-password">Password</label>
+                                <label for="login-password">{{ __('Password') }}</label>
                                 <a href="{{ url("auth/forgot-password-v2" )}}">
-                                    <small>Forgot Password?</small>
+                                    <small>{{ __('Forgot Password') }} ?</small>
                                 </a>
                             </div>
 
@@ -77,23 +81,17 @@
                         <div class="form-group">
                             <div div class="custom-control custom-checkbox">
                                 <input class="custom-control-input" id="remember-me" type="checkbox" tabindex="3" />
-                                <label class="custom-control-label" for="remember-me">Remember Me</label>
+                                <label class="custom-control-label" for="remember-me">
+                                    {{ __('Remember me') }}
+                                </label>
                             </div>
                         </div>
 
-                        <button class="btn btn-primary btn-block" tabindex="4">Sign in</button>
+                        <button class="btn btn-primary btn-block" tabindex="4">{{ __('Sign in') }}</button>
                     </form>
                 </div>
             </div>
             <!-- /Login-->
         </div>
     </div>
-@endsection
-
-@section('vendor-script')
-<script src="{{asset(mix('vendors/js/forms/validation/jquery.validate.min.js'))}}"></script>
-@endsection
-
-@section('page-script')
-<script src="{{asset(mix('js/scripts/pages/page-auth-login.js'))}}"></script>
 @endsection
