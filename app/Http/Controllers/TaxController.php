cfcfa031d96b89dab8ae3c3883757ebb9a1e19c9
John Rexter Flores <johnrexter.flores@gmail.com>
2021-08-27 03:43:55 +0800

    Refactor tax controller

--- app/Http/Controllers/TaxController.php
+++ app/Http/Controllers/TaxController.php
@@ -3,56 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 
-use App\Models\Company;
 use App\Exports\TaxExport;
 
 class TaxController extends Controller
 {
     public function index()
     {
-    	$response = Gate::inspect('tax.view');
+    	$this->authorize('tax.view');
 
     	$breadcrumbs = [
 	        ['link'=> route('home'), 'name'=>"Dashboard"], 
 	        ['name'=>"Tax"],
 	    ];
 
-	    $company = Company::getCompanyDetails();
-
-		if ( $response->allowed() ) {
-		    return view('tax.index', [
-		    	'breadcrumbs' => $breadcrumbs,
-		    	'company'     => $company
-		    ]);
-		} else {
-		    return view('misc.not-authorized');
-		}
+		return view('tax.index', [
+	    	'breadcrumbs' => $breadcrumbs,
+	    	'company'     => $this->company
+	    ]);
     }
 
     public function export(Request $request)
     {
+    	$this->authorize('tax.export');
+
     	$types = ['csv', 'pdf', 'xlsx', 'xls', 'ods'];
 
     	$requested_type = isset($request['type']) ? strtolower($request['type']) : 'csv';
     	$q = $request['q'];
 
-    	$response = Gate::inspect('tax.export');
+    	// Set default type, if specified type is invalid
+    	if ( !in_array($requested_type, $types) ) {
+    		$requested_type = 'csv';
+    	}
 
-    	$company = Company::getCompanyDetails();
-
-    	if ( $response->allowed() ) {
-		    
-		    // Set default type, if specified type is invalid
-	    	if ( !in_array($requested_type, $types) ) {
-	    		$requested_type = 'csv';
-	    	}
-
-	    	return (new TaxExport($q, $company->id))
-	    			->download('tax-' . now()->format('Y-m-d') . '.' . $requested_type);
-		} else {
-		    return view('misc.not-authorized');
-		}
+    	return (new TaxExport($q, $this->company->id))->download('tax-' . now()->format('Y-m-d') . '.' . $requested_type);
     }
 }
