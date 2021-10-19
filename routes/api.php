85c15f4af17646e9058301ea088d9d94f28f9338
John Rexter Flores <johnrexter.flores@gmail.com>
2021-10-19 13:19:26 +0800

    Update route name

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', 'Api\LoginController@authenticate');
+Route::post('signin', 'Api\LoginController@authenticate');
 
 Route::middleware('auth:sanctum')->group(function() {
 
