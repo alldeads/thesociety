2f43d230fa514d0240ee6c322f8bc7969fb28cb2
John Rexter Flores <johnrexter.flores@gmail.com>
2021-11-24 23:13:33 +0800

    Remove unused code

--- app/Http/Livewire/User/Setting.php
+++ app/Http/Livewire/User/Setting.php
@@ -6,13 +6,6 @@ use Livewire\Component;
 
 class Setting extends Component
 {
-	public $configData;
-
-	public function switch()
-	{
-
-	}
-
     public function render()
     {
         return view('livewire.user.setting');
