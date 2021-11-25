f838a5fd95411f38e5f4eeff3aacb64ebd93a8ed
John Rexter Flores <johnrexter.flores@gmail.com>
2021-11-26 03:37:58 +0800

    Remove unused code

--- app/Http/Controllers/Accounting/CashFlowController.php
+++ app/Http/Controllers/Accounting/CashFlowController.php
@@ -50,17 +50,6 @@ class CashFlowController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
