c33ca80bf182cc58e444e2094c5f639c7ed541b0
John F <johnrexter.flores@gmail.com>
2022-12-19 00:36:52 +0800

    Remove unused icon in the sidebar

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -10,12 +10,6 @@
                     <img src="{{ $__avatar }}" width="50">
                 </a>
             </li>
-            <li class="nav-item nav-toggle">
-                <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse">
-                    <i class="d-block d-xl-none text-primary toggle-icon font-medium-4" data-feather="x"></i>
-                    <i class="d-none d-xl-block collapse-toggle-icon font-medium-4  text-primary" data-feather="disc" data-ticon="disc"></i>
-                </a>
-            </li>
         </ul>
     </div>
     
